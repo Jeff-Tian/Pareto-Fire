9cdded488c63bbd93466689e7cd3bf83a72260e0
Jeff Tian <jeff.tian@outlook.com>
2018-09-01 10:59:11 +0800

    Fixed bottom menu

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import 'semantic-ui-css/semantic.min.css'
 
 import Header from '../components/header'
 import './index.css'
+import { Container, Icon, Menu } from 'semantic-ui-react'
 
+const activeItem = '发布借款'
+const handleClick = () => {
+  alert(arguments)
+  console.log(arguments)
+}
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: '开放银行的信用贷款平台' },
+        { name: 'keywords', content: '信用贷款, 开放银行' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <Header siteTitle={data.site.siteMetadata.title}/>
+    <Container>
       {children()}
-    </div>
+      <Menu tabular fluid widths={3} icon="labeled" fixed="bottom">
+        <Menu.Item name="借款历史" active={activeItem === '借款历史'} onClick={handleClick}>
+          <Icon name="history"/>
+          借款历史
+        </Menu.Item>
+        <Menu.Item name="发布借款" active={activeItem === '借款历史'} onClick={handleClick}>
+          <Icon name="add circle"/>
+          发布借款</Menu.Item>
+        <Menu.Item name="设置" active={activeItem === '借款历史'} onClick={handleClick}>
+          <Icon name="settings"/>
+          设置
+        </Menu.Item>
+      </Menu>
+    </Container>
   </div>
 )
 
