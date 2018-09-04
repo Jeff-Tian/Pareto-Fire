ba14d087f6ef08fba1aab792aea8eddb65cf3b96
Jeff Tian <jeff.tian@outlook.com>
2018-09-04 12:06:15 +0800

    Fix build error

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,16 +2,20 @@ import { Icon, Menu } from 'semantic-ui-react'
 import Link from 'gatsby-link'
 import React from 'react'
 
-export default () => <Menu tabular fluid widths={3} icon="labeled" inverted>
-  <Menu.Item as={Link} name="借款历史" active={window.location.pathname === '/history'} to="/history" color="red">
-    <Icon name="history"/>
-    借款历史
-  </Menu.Item>
-  <Menu.Item as={Link} name="发布借款" active={window.location.pathname === '/'} to="/" color="red">
-    <Icon name="add circle"/>
-    发布借款</Menu.Item>
-  <Menu.Item as={Link} name="设置" active={window.location.pathname === '/settings'} to="/settings" color="red">
-    <Icon name="settings"/>
-    设置
-  </Menu.Item>
-</Menu>
+export default () => {
+  const pathname = typeof window === 'undefined' ? '' : window.location.pathname
+
+  return <Menu tabular fluid widths={3} icon="labeled" inverted>
+    <Menu.Item as={Link} name="借款历史" active={pathname === '/history'} to="/history" color="red">
+      <Icon name="history"/>
+      借款历史
+    </Menu.Item>
+    <Menu.Item as={Link} name="发布借款" active={pathname === '/'} to="/" color="red">
+      <Icon name="add circle"/>
+      发布借款</Menu.Item>
+    <Menu.Item as={Link} name="设置" active={pathname === '/settings'} to="/settings" color="red">
+      <Icon name="settings"/>
+      设置
+    </Menu.Item>
+  </Menu>
+}
