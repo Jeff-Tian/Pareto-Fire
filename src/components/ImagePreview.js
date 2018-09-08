b40a8dc61a034f8e606141e12679a3b10c509f54
Jeff Tian <jeff.tian@outlook.com>
2018-09-08 19:03:43 +0800

    Image preview for history

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Image } from 'semantic-ui-react'
 
 export default ({ images, deleteImage }) => <Image.Group size='tiny'>
-  {images && images.map(img => <Image key={img.name} src={window.URL.createObjectURL(img)}
-                                      label={{
+  {images && images.map(img => <Image key={img.name || img.filename} src={img.url || window.URL.createObjectURL(img)}
+                                      label={deleteImage ? {
                                         as: 'a',
                                         corner: 'right',
                                         icon: 'remove circle',
                                         onClick: () => deleteImage(img),
-                                      }}
+                                      } : null}
   />)}
 </Image.Group>
--- src/pages/history.js
+++ src/pages/history.js
@@ -4,6 +4,7 @@ import 'regenerator-runtime/runtime'
 import moment from 'moment/moment'
 import Link from 'gatsby-link'
 import CheckUser from '../components/CheckUser'
+import ImagePreview from '../components/ImagePreview'
 
 class HistoryPage extends React.Component {
   constructor(props) {
@@ -20,23 +21,46 @@ class HistoryPage extends React.Component {
     CheckUser.init()
     let user = CheckUser.checkUser()
 
+    this.setState({ loading: true })
     try {
       let res = await window.fetch(`https://api.netlify.com/api/v1/submissions?access_token=d0fb464bf842b9f97a361a8077893483a4dcf20837cf4cc679f22ef0eebea81d`)
 
-      this.setState({ history: (await res.json()).filter(loan => loan.data.userId === user.id) })
+      this.setState({
+        history: (await res.json()).filter(loan => loan.data.userId === user.id).map(loan => {
+          loan.files = []
+          const indices = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+          indices.map(i => {
+            let f = loan.data[`files_${i}`]
+            if (f) {
+              loan.files.push(f)
+            }
+          })
+          return loan
+        }),
+      })
     } catch (ex) {
       console.error(ex)
+    } finally {
+      this.setState({ loading: false })
     }
   }
 
   render() {
     return <Card.Group>
       {
-        this.state.history.map(s => <Card as={Link} fluid color='red' header={`￥ ${s.data.howMuch} 元`}
+        this.state.history.map(s => <Card as={Link} fluid color='red'
                                           to={`/?loan_id=${s.id}`}
-                                          meta={moment(s.created_at).fromNow()}
-                                          description={s.data.scheme} key={s.id}>
-        </Card>)
+                                          key={s.id}>
+            <Card.Content>
+              <Card.Header>{`￥ ${s.data.howMuch} 元`}</Card.Header>
+              <Card.Meta>{moment(s.created_at).fromNow()}</Card.Meta>
+              <Card.Description>{s.data.scheme}</Card.Description>
+            </Card.Content>
+            <Card.Content>
+              <ImagePreview images={s.files}/>
+            </Card.Content>
+          </Card>,
+        )
       }
     </Card.Group>
   }
