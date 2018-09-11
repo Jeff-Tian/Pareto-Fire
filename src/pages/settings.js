b7e59d2805f05023df4f6df88c2d3329f4ab47e1
Jeff Tian <jie.tian@unicareer.com>
2018-09-11 18:32:39 +0800

    redirect to citi oauth page

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -18,7 +18,6 @@ export default class Settings extends React.Component {
     const user = CheckUser.checkUser()
 
     this.setState({ user: user })
-    console.log('user = ', user)
   }
 
   render() {
@@ -35,7 +34,8 @@ export default class Settings extends React.Component {
             </Item.Meta>
             <Item.Description>注册于：{moment(this.state.user.created_at).fromNow()}</Item.Description>
             <Item.Extra>
-              <Button color="facebook">绑定花旗账号</Button>
+              <a className="ui facebook button"
+                 href={`https://sandbox.apihub.citi.com/gcb/api/authCode/oauth2/authorize?response_type=code&client_id=61e8bab0-a650-4dd6-818d-245e220798b7&scope=customers_profiles&countryCode=US&businessCode=GCB&locale=en_US&state=opaqueStateValue&redirect_uri=https://fire.pa-pa.me`}>绑定花旗账号</a>
               {/*<Label>IMAX</Label>*/}
               {/*<Label icon='globe' content='Additional Languages'/>*/}
             </Item.Extra>
