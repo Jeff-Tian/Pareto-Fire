df6e3a45e1f1e713d857836bf88a14240d1730f9
Jeff Tian <jie.tian@unicareer.com>
2018-09-11 20:02:12 +0800

    only allow post

--- functions/service-proxy.js
+++ functions/service-proxy.js
@@ -1,5 +1,9 @@
-exports.handler = function(event, context, callback) {
+exports.handler = async (event, context, callback) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
   const { identity, user } = context.clientContext
+  const { uri } = event.queryStringParameters
   callback(null, {
     statusCode: 200,
     body: 'Hello, World',
