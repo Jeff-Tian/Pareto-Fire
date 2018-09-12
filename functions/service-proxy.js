fd9abcf76c48ffe74024a83c791dcb580a83c519
Jeff Tian <jie.tian@unicareer.com>
2018-09-12 11:00:50 +0800

    error handling

--- functions/service-proxy.js
+++ functions/service-proxy.js
@@ -4,14 +4,21 @@ exports.handler = async (event, context, callback) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
   }
-  const { identity, user } = context.clientContext
-  const { uri } = event.queryStringParameters
-  const body = querystring.parse(event.body)
-  callback(null, {
-    statusCode: 200,
-    uri: uri,
-    user: user,
-    identity: identity,
-    body: body,
-  })
+  try {
+    const { identity, user } = context.clientContext
+    const { uri } = event.queryStringParameters
+    const body = querystring.parse(event.body)
+
+    callback(null, {
+      statusCode: 200,
+      uri: uri,
+      user: user,
+      identity: identity,
+      body: body,
+    })
+  } catch (ex) {
+    console.error(ex)
+
+    callback(ex)
+  }
 }
