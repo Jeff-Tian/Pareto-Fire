7a705f5a8a8e1eb34d4d6b0bfb0cc6aa0084cc66
Jeff Tian <jie.tian@unicareer.com>
2018-09-12 10:20:06 +0800

    hello lambda

--- functions/service-proxy.js
+++ functions/service-proxy.js
@@ -1,17 +1,8 @@
 import querystring from 'querystring'
 
 exports.handler = async (event, context, callback) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' }
-  }
-  const { identity, user } = context.clientContext
-  const { uri } = event.queryStringParameters
-  const body = querystring.parse(event.body)
   callback(null, {
     statusCode: 200,
-    uri: uri,
-    user: user,
-    identity: identity,
-    body: body,
+    body: 'hello',
   })
 }
