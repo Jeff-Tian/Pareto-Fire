eec1f37c968df5abe8f2c330fa1ea59fb7ae4056
Jeff Tian <jie.tian@unicareer.com>
2018-09-12 14:01:46 +0800

    don't use querystring

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,26 +1,7 @@
-import querystring from 'querystring'
-
 exports.handler = async (event, context, callback) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
-  }
-
-  try {
-    const { uri } = event.queryStringParameters
-    const body = querystring.parse(event.body)
-
-    return {
-      statusCode: 200,
-      uri: uri,
-      body: body,
-    }
-  } catch (ex) {
-    console.log('error:', ex)
-    console.error(ex)
-
-    return {
-      statusCode: 422,
-      body: ex,
-    }
+  } else {
+    return { statusCode: 200, body: 'posted' }
   }
 }
